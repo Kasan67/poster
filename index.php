756a3027a2eeb657e20fbfc90177c61e424e712e
kashirinsan <kashirinas1991@gmail.com>
2016-07-07 19:30:00 +0300

    patterns

--- index.php
+++ index.php
@@ -1,30 +1,6 @@
 <?php
 
-class Db
-{
-    private $db;
-    private static $_instance;
-    private function __construct()
-    {
-        $config = parse_ini_file('database.ini');
-        $this->db = new mysqli($config['hosts'], $config['username'], $config['password'], $config['database']);
-    }
-    public static function getInstance()
-    {
-        if(!self::$_instance){
-            self::$_instance = new self();
-            return self::$_instance;
-        }else{
-            return self::$_instance;
-        }
-    }
-
-    private function __clone(){}
-    public function getConnect()
-    {
-        return $this->db;
-    }
-}
+include_once('Db.php');
 
 ?>
 
